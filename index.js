27888361d49b31e35c49e7a0275bc15e60f63165
Al-amin Sheikh <azalamins@gmail.com>
2022-06-17 14:43:22 +0600

    reset password api done 50%

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
 
+const port = process.env.PORT || 5000;
 const app = express();
+
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-
 app.use(cors());
 app.use(express.json());
 
@@ -18,6 +20,15 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+};
+
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
 async function run() {
   try {
@@ -85,18 +96,14 @@ async function run() {
     });
 
     // Forget Password API
-    app.get("/forgot-password", (req, res, next) => {
-      res.render("forgot-password");
-    });
-
     app.post("/forgot-password", async (req, res, next) => {
-      const { email } = req.body;
+      const user = req.body;
       const existEmail = await registerUserCollection.findOne({
-        email: email,
+        email: user?.email,
       });
 
       if (existEmail == null) {
-        res.send("User not register");
+        res.send({ status: false, message: "User not register" });
         return;
       }
 
@@ -107,16 +114,82 @@ async function run() {
         id: existEmail?._id,
       };
       const token = jwt.sign(payload, secret, { expiresIn: "15m" });
-      const link = `http://localhost:5000/reset-password/${existEmail?._id}/${token}`;
+      const link = `http://localhost:5000/forgot-password/${existEmail?.email}/${token}`;
       console.log(link);
+
       res.send({
-        status: 200,
-        message: "Password reset link has been sent to your email",
+        status: true,
+        message: "Password reset link has been sent to your email...",
       });
+
+      nodemailerMailgun.sendMail(
+        {
+          from: "Programminghero@email.com",
+          to: existEmail?.email,
+          subject: "Reset Password",
+          html: `<div>
+            <p>Please reset your password by the below link</p>
+            <p>
+                <a href=${link} target='_blank'>Click here</a>
+            </p>
+          </div>`,
+        },
+        (err, info) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(info);
+          }
+        }
+      );
     });
 
-    app.get("/forgot-password", (req, res, next) => {});
-    app.post("/forgot-password", (req, res, next) => {});
+    app.get("/forgot-password/:email/:token", async (req, res, next) => {
+      const { email, token } = req.params;
+      const existEmail = await registerUserCollection.findOne({
+        email: email,
+      });
+      // check this id exist in database
+      console.log(existEmail?.email);
+      if (email !== existEmail?.email) {
+        res.send("Invalid Email ...");
+        return;
+      }
+      // we have a valid id and valid user with this id
+      const secret = process.env.JWT_SECRET + existEmail?.password;
+      try {
+        const payload = jwt.verify(token, secret);
+        res.render("reset-password", { email: existEmail?.email });
+      } catch (error) {
+        console.log(error);
+        res.send(error?.message);
+      }
+    });
+
+    app.post("/forgot-password/:email/:token", async (req, res, next) => {
+      const { email, token } = req.params;
+      const { password, password2 } = req.body;
+      const existEmail = await registerUserCollection.findOne({
+        email: email,
+      });
+
+      // check this email exist in database
+      if (email !== existEmail?.email) {
+        res.send("Invalid Email...");
+        return;
+      }
+
+      const secret = process.env.JWT_SECRET + existEmail?.password;
+
+      try {
+        const payload = jwt.verify(token, secret);
+        existEmail.password = password2;
+        res.send(existEmail);
+      } catch (error) {
+        console.log(error);
+        res.send(error?.message);
+      }
+    });
   } finally {
     // await client.close();
   }
